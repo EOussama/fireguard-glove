5072c90b01db005e9e93a8b000644561bfee5999
EOussama <oussama_gtacj@live.fr>
2024-01-21 00:21:23 +0100

    feat: types docs, #8

--- src/types/firemitt-options.type.ts
+++ src/types/firemitt-options.type.ts
@@ -1,5 +1,17 @@
 import { TDim, TFireguardOptions, TPos } from '.';
 
+/**
+ * @description
+ * Options for configuring Firemitt.
+ * This type outlines the optional and partial settings that can be applied to customize the Firemitt.
+ * 
+ * @type {TFiremittOptions}
+ * 
+ * @property {string} url - The URL associated with the Firemitt.
+ * @property {Partial<TPos>} [pos] - Optional, partial position configuration.
+ * @property {Partial<TDim>} [dim] - Optional, partial dimension configuration.
+ * @property {Partial<TFireguardOptions>} [config] - Optional, partial Fireguard configuration.
+ */
 export type TFiremittOptions = { url: string } & Partial<{
   pos: Partial<TPos>
   dim: Partial<TDim>
