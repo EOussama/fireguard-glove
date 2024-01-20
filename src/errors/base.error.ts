c879f93830eeeb2b22de9e97bac1217a56316ee9
EOussama <oussama_gtacj@live.fr>
2024-01-21 00:11:27 +0100

    feat: error docs, #8

--- src/errors/base.error.ts
+++ src/errors/base.error.ts
@@ -1,8 +1,26 @@
+/**
+ * @description
+ * Custom error class that extends the built-in Error class.
+ * It is used as a base class for creating custom error types in your application.
+ */
 export class BaseError extends Error {
+
+  /**
+   * @description
+   * Creates a new instance of the BaseError class with the specified error message.
+   *
+   * @param message The error message describing the reason for the error.
+   */
   constructor(message: string) {
     super(message);
   }
 
+  /**
+   * @description
+   * Returns a string representation of the error message, prefixed with "[ERROR]".
+   *
+   * @returns A formatted error message.
+   */
   toString() {
     return `[ERROR] ${this.message}.`;
   }
--- src/errors/invalid-app.error.ts
+++ src/errors/invalid-app.error.ts
@@ -1,6 +1,18 @@
 import { BaseError } from './base.error';
 
+/**
+ * @description
+ * Custom error class representing an error that occurs when Firebase initialization fails
+ * due to an invalid configuration.
+ * This error is thrown when there is an issue with the provided Firebase configuration.
+ */
 export class InvalidAppError extends BaseError {
+
+  /**
+   * @description
+   * Creates a new instance of the InvalidAppError class with a default error message.
+   * The default error message indicates that Firebase initialization failed due to an invalid configuration.
+   */
   constructor() {
     super('Invalid Firebase app, failed initializing fire base with the given configuration');
   }
