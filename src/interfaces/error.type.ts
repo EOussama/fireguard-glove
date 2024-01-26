041b45ff835daf4275bee83948f698271fdc621c
eoussama <oussama.essamadi@gmail.com>
2024-01-26 22:26:05 +0100

    feat: error serialization, #53

---
+++ src/interfaces/error.type.ts
@@ -0,0 +1,30 @@
+import { ErrorType } from '..';
+
+/**
+ * @category Firemitt
+ * 
+ * @description
+ * Custom error type.
+ * 
+ * @type {IError}
+ */
+export interface IError {
+
+  /**
+   * @description
+   * The name of the error.
+   */
+  name: string
+
+  /**
+   * @description
+   * The error message.
+   */
+  message: string
+
+  /**
+   * @description
+   * The type of error.
+   */
+  type: ErrorType
+}
